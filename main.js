db8fdbf997b56b556b7777492b010b2abb2d1576
william-hayward <williamhayward2@gmail.com>
2022-04-14 16:39:57 +0000

    part d & e, task 7, 8 & 9 complete.

--- main.js
+++ main.js
@@ -1,55 +1,123 @@
 // express server variables:
-const express = require("express");
+const express = require('express');
 const app = express();
 
 // connection variables:
 const con = require('./mysqlconn');
 const mysql = require('mysql2');
 
+// session variables:
+const expressSession = require('express-session');
+const MySQLStore = require('express-mysql-session')(expressSession);
+const sessionStore = new MySQLStore({}, con.promise());
+
 app.use(express.json());
 app.use(express.static('public'));
 
-app.get("/", (req, res) => {
-	console.log("working"); // test purposes
+app.use(
+	expressSession({
+		store: sessionStore,
+		secret: 'PlacesToStay',
+		resave: false,
+		saveUninitialized: false,
+		rolling: true,
+		unset: 'destroy',
+		proxy: true,
+		cookie: {
+			maxAge: 600000,
+			httpOnly: false,
+		},
+	})
+);
+
+app.post('/login', (req, res) => {
+	con.query(
+		`SELECT * FROM acc_users WHERE username=? AND password=?`,
+		[req.body.username, req.body.password],
+		(error, results, fields) => {
+			if (results.length == 1) {
+				req.session.username = req.body.username;
+				res.json({ username: req.body.username });
+			} else {
+				res.status(401).json({ error: error });
+			}
+		}
+	);
 });
 
-app.get("/accommodation/:location", (req, res) => {
-	con.query(`SELECT * FROM accommodation WHERE location=?`,
-    [req.params.location], (error,results,fields) => { 
-        if(error) {
-            res.status(500).json({ error: error });
-        } else {
-            res.json(results);
-        }
-    });
+app.post('/logout', (req, res) => {
+	req.session = null;
+	res.json({ success: 1 });
 });
 
-app.get("/accommodation/:location/type/:type", (req, res) =>{
-	con.query(`SELECT * FROM accommodation WHERE location=? AND type =?`,
-    [req.params.location, req.params.type], (error,results,fields) => { 
-        if(error) {
-            res.status(500).json({ error: error });
-        } else {
-            res.json(results);
-        }
-    });
+app.use((req, res, next) => {
+	if (['POST', 'DELETE'].indexOf(req.method) == -1) {
+		next();
+	} else {
+		if (req.session.username) {
+			next();
+		} else {
+			res.status(401).json({ error: "You're not logged in. Go away!" });
+		}
+	}
+});
+
+app.get('/login', (req, res) => {
+	res.json({ username: req.session.username || null });
+});
+
+app.get('/accommodation/:location', (req, res) => {
+	con.query(
+		`SELECT * FROM accommodation WHERE location=?`,
+		[req.params.location],
+		(error, results, fields) => {
+			if (error) {
+				res.status(500).json({ error: error });
+			} else {
+				res.json(results);
+			}
+		}
+	);
+});
+
+app.get('/accommodation/:location/type/:type', (req, res) => {
+	con.query(
+		`SELECT * FROM accommodation WHERE location=? AND type =?`,
+		[req.params.location, req.params.type],
+		(error, results, fields) => {
+			if (error) {
+				res.status(500).json({ error: error });
+			} else {
+				res.json(results);
+			}
+		}
+	);
 });
 
 // {"accID": 1, "thedate": 220601, "username": "testusername", "npeople": 5}
-app.post("/book", (req, res) =>{
-	con.query(`INSERT INTO acc_bookings(accID, thedate, username, npeople) VALUES(?,?,?,?);
-	UPDATE acc_dates SET availability = availability - 1 WHERE accID = ? AND thedate = ?`,
-			 [req.body.accID, req.body.thedate, req.body.username, req.body.npeople , req.body.accID, req.body.thedate],
-			 function(error, results, fields){
-		if (error) {
-      		return con.rollback(function() {
-        		res.status(500).json({ error: error });
-			});
-		}else{
-			res.json("successfully booked.");
+app.post('/book', (req, res) => {
+	con.query(
+		`INSERT INTO acc_bookings(accID, thedate, username, npeople) VALUES(?,?,?,?);
+	UPDATE acc_dates SET availability = availability - ? WHERE accID = ? AND thedate = ?`,
+		[
+			req.body.accID,
+			req.body.thedate,
+			req.body.username,
+			req.body.npeople,
+			req.body.npeople,
+			req.body.accID,
+			req.body.thedate,
+		],
+		function (error, results, fields) {
+			if (error) {
+				return con.rollback(function () {
+					res.status(500).json({ error: error });
+				});
+			} else {
+				res.json('successfully booked.');
+			}
 		}
-	});
+	);
 });
-
 
 app.listen(3000);
--- public/index.js
+++ public/index.js
@@ -1,3 +1,11 @@
+const map = L.map('map1');
+
+const attrib = 'Map data copyright OpenStreetMap contributors, Open Database Licence';
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	attribution: attrib,
+}).addTo(map);
+
 async function searchAccomodation(location) {
 	const search = await fetch(`/accommodation/${location}`);
 	const accomodations = await search.json();
@@ -16,7 +24,7 @@ async function searchAccomodation(location) {
 			const accom = {
 				accID: location.ID,
 				thedate: 220601,
-				username: "tempUsername",
+				username: 'tempUsername',
 				npeople: 2,
 			};
 			const BookAccomodation = await fetch(`/book`, {
@@ -28,6 +36,8 @@ async function searchAccomodation(location) {
 			});
 			if (BookAccomodation.status == 200) {
 				alert('Accomodation booked successfully');
+			} else if (BookAccomodation.status === 401) {
+				alert('Error. user is not logged in.');
 			} else {
 				const json = await response.json();
 				alert(`Error booking accomodation: details ${json.error}`);
@@ -35,6 +45,11 @@ async function searchAccomodation(location) {
 		});
 		document.getElementById('results').appendChild(p);
 		document.getElementById('results').appendChild(btn);
+
+		const pos = [location.latitude, location.longitude];
+		map.setView(pos, 8);
+		const locationMarker = L.marker(pos).addTo(map);
+		locationMarker.bindPopup(`Name: ${location.name}, Description: ${location.description}`);
 	});
 }
 
@@ -43,4 +58,46 @@ async function searchAccomodation(location) {
 document.getElementById('Search!').addEventListener('click', () => {
 	const location = document.getElementById('location').value;
 	searchAccomodation(location);
+});
+
+function login(username, password) {
+	const user = {
+		username: username,
+		password: password,
+	};
+
+	const response = fetch('/login', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+		},
+		body: JSON.stringify(user),
+	}).then((response) => {
+		if (response.status == 401) {
+			document.getElementById('loginMessage').innerHTML = 'Wrong Login details';
+		} else {
+			console.log('Login Complete! response:', response);
+			document.getElementById('loginMessage').innerHTML = `
+			Logged in as ${username}! <input type='button' value='Logout' id='logoutBtn' />`;
+
+			document.getElementById('logoutBtn').addEventListener('click', () => {
+				const logoutResponse = fetch('/logout', {
+					method: 'POST',
+				}).then((logoutResponse) => {
+					if (logoutResponse.status == 200) {
+						console.log('logout complete.');
+						document.getElementById('loginMessage').innerHTML = `logout successful.`;
+					} else {
+						alert('Error! Something went wrong.');
+					}
+				});
+			});
+		}
+	});
+}
+
+document.getElementById('loginBtn').addEventListener('click', () => {
+	const username = document.getElementById('Username').value;
+	const password = document.getElementById('Password').value;
+	login(username, password);
 });
